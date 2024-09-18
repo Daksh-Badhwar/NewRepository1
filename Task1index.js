acc653c4feb6b0d5f453f1190b90ad5bd6cfb00c
Daksh-Badhwar <badhwardakshhwhlpj369@gmail.com>
2024-09-18 19:22:13 +0530

    bringing code to git

---
+++ Task1index.js
@@ -0,0 +1,8 @@
+document.getElementById("main-heading").textContent="Fruit World";
+document.getElementById("main-heading").style.color="Orange";
+document.getElementById("header").style.backgroundColor="green";
+document.getElementById("header").style.borderBottom="3px solid orange";
+document.getElementById("basket-heading").style.color="green";
+document.getElementById("basket-heading").style.fontStyle="italic";
+document.getElementById("thanks").innerHTML="<p> Pls visit us again </p>";
+document.getElementById("thanks").style.fontWeight="bold";
